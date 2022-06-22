e35f10203f397d0521feaf5915ffe38ccf76209c
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-06-22 15:46:26 -0300

    atualizando

--- modulo03/projeto-final/db/db_store.sql
+++ modulo03/projeto-final/db/db_store.sql
@@ -14,10 +14,19 @@ CREATE TABLE tb_product (
     name VARCHAR(30) NOT NULL,
     description VARCHAR(100) NOT NULL,
     photo VARCHAR(255) NOT NULL,
-    valor FLOAT(5,2) not null,
-    categoria_id INT(11) NOT NULL,
+    value FLOAT(5,2) not null,
+    category_id INT(11) NOT NULL,
     quanty INT(5) NOT NULL,
-    create_at DATETIME NOT NULL
+    created_at DATETIME NOT NULL
+);
+
+CREATE TABLE  tb_client (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    cpf CHAR(11) NOT NULL,
+    dataNascimento DATE,
+    sexo CHAR(1) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
 
 INSERT INTO tb_category (name, description)
@@ -26,10 +35,12 @@ VALUES
 ('Escritorio', 'Canetas, Cadernos, follhas, etc'),
 ('Eletronicos', 'TVs, Som portatil, Caixas de som');
 
+INSERT INTO tb_client (name, cpf, dataNascimento, sexo, email)
+
 -- verifica as colunas dos produtos cadastradas--
 DESC td_product;
 
 INSERT INTO tb_product (name, description, photo, value, category_id, quantity, created_at)
 VALUES 
-
-<!--('Monitor Gamer LED 31,5', 'widescreen curvo 1ms 165hz Agon AG323FCXE Aoc CX 1 UN', 'https://img.kalunga.com.br/fotosdeprodutos/477151z_1.jpg?_gl=1*1v3kgh7*_gcl_aw*R0NMLjE2NTU1MTY5MjEuQ2owS0NRand6TENWQmhEM0FSSXNBUEtZVGNUMnVLS1M3RDhFcHdrSHhoMXZMLXUyUlNkdThpSHZ4VEVweGtFbzhWTk1rc050X2RFNVZsZ2FBbkgwRUFMd193Y0I.&_ga=2.110817669.10191330.1655516921-1605343242.1655516921&_gac=1.229553518.1655516921.Cj0KCQjwzLCVBhD3ARIsAPKYTcT2uKKS7D8EpwkHxh1vL-u2RSdu8iHvxTEpxkEo8VNMksNt_dE5VlgaAnH0EALw_wcB', 2.298.80, 1, 9),-->
+('Teclado', 'Teclado Bla bla', 'https://m.media-amazon.com/images/I/616Lyy60cTL._AC_SL1200_.jpg', 199.89, 1, 50 '2022-06-21 21:34:20'),
+('Teclado 2', 'Teclado Bla bla', 'https://m.media-amazon.com/images/I/616Lyy60cTL._AC_SL1200_.jpg', 199.89, 1, 50 '2022-06-21 21:34:20');
---
+++ modulo03/projeto-final/src/View/_partials/head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lojinha Virtual - Admin</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body class="container" style="background-color:hotpink"> <!--tela inteira-->
+<nav class="mt-3">    
+  <a href="/" class="btn btn-outline-dark">Inicio</a>
+  <a href="/categorias" class="btn btn-outline-dark">Categorias</a>
+  <a href="/produtos" class="btn btn-outline-dark">Produtos</a>
+</nav>
+
+<hr>
